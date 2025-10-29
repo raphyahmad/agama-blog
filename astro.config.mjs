4a46d43f8ae359e8071f71ea84b5ab59f18dd034
raphyahmad <rafiahmadzaini5@gmail.com>
2025-10-29 21:51:17 +0700

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://raphyahmad.github.io",
+	base: "/agama-blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
